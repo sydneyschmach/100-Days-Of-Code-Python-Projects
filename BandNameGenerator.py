7f14c56928d3f1cd546d533f75593017670fe931
sydneyschmach <sydneystevenson95@icloud.com>
2022-06-04 15:21:36 -0600

    Day 1 of 100 Days of Python, Band Name Generator

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator!")
+cityLivedIn = input("What City did you grow up in?\n")
+nameOfPet = input("What is the name of your pet?\n")
+print(cityLivedIn + " " + nameOfPet)
